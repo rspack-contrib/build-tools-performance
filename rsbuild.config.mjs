4aa93da151e2c65ff0f529aacabc26030a2de0b7
neverland <chenjiahan.jait@bytedance.com>
2024-07-05 13:55:03 +0800

    chore: bump Rspack / Rsbuild

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,9 +1,7 @@
+// @ts-check
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
   plugins: [pluginReact()],
-  output: {
-    polyfill: "off",
-  },
 });
