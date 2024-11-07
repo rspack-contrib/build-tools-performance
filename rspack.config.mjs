e29eb40c573739b9737e5191f6b6a2c4d67dccf4
hardfist <yangjianzju@gmail.com>
2024-11-07 23:08:59 +0800

    chore: disable lazy in production

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -49,6 +49,6 @@ export default defineConfig({
   ].filter(Boolean),
   experiments: {
     css: true,
-    lazyCompilation:true
+    lazyCompilation: !isProduction // lazyCompilation should only be enabled in development mode
   },
 });
