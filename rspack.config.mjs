552d08157e52fda4d400b818a75b28777af0d313
hardfist <yangjianzju@gmail.com>
2024-11-07 22:41:02 +0800

    chore: adjust rspack target to esnext

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -25,6 +25,7 @@ export default defineConfig({
           loader: "builtin:swc-loader",
           options: {
             jsc: {
+              target: 'esnext',
               parser: {
                 syntax: "typescript",
                 tsx: true,
