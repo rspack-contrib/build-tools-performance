ab846ed6ef3bbec999f5c26f296fb6bf98df357e
neverland <chenjiahan.jait@bytedance.com>
2025-04-25 14:15:25 +0800

    chore: setup prettier

--- benchmark.mjs
+++ benchmark.mjs
@@ -1,9 +1,9 @@
-import { spawn } from "child_process";
-import { rmSync, appendFile, readFileSync, writeFileSync } from "fs";
-import { createRequire } from "module";
-import path from "path";
-import puppeteer from "puppeteer";
-import kill from "tree-kill";
+import { spawn } from 'child_process';
+import { rmSync, appendFile, readFileSync, writeFileSync } from 'fs';
+import { createRequire } from 'module';
+import path from 'path';
+import puppeteer from 'puppeteer';
+import kill from 'tree-kill';
 import { logger } from 'rslog';
 import color from 'picocolors';
 
@@ -13,39 +13,50 @@ const __dirname = import.meta.dirname;
 const startConsole = "console.log('Benchmark Start Time', Date.now());";
 const startConsoleRegex = /Benchmark Start Time (\d+)/;
 
-const caseName = process.env.CASE ?? "medium";
+const caseName = process.env.CASE ?? 'medium';
 
 process.env.CASE = caseName;
 
 class BuildTool {
-  constructor({ name, port, startScript, startedRegex, buildScript, binFilePath }) {
+  constructor({
+    name,
+    port,
+    startScript,
+    startedRegex,
+    buildScript,
+    binFilePath,
+  }) {
     this.name = name;
     this.port = port;
     this.startScript = startScript;
     this.startedRegex = startedRegex;
     this.buildScript = buildScript;
-    this.binFilePath = path.join(process.cwd(), "node_modules", binFilePath);
+    this.binFilePath = path.join(process.cwd(), 'node_modules', binFilePath);
     this.hackBinFile();
   }
 
   cleanCache() {
     try {
-      rmSync("./node_modules/.cache", { recursive: true, force: true });
-      rmSync("./node_modules/.vite", { recursive: true, force: true });
-      rmSync("./node_modules/.farm", { recursive: true, force: true });
-    } catch (err) { }
+      rmSync('./node_modules/.cache', { recursive: true, force: true });
+      rmSync('./node_modules/.vite', { recursive: true, force: true });
+      rmSync('./node_modules/.farm', { recursive: true, force: true });
+    } catch (err) {}
   }
 
   // Add a `console.log('Benchmark start', Date.now())` to the bin file's second line
   hackBinFile() {
-    logger.info("Setup bin file for", color.green(this.name), color.dim(`(${this.binFilePath.split('node_modules/')[1]})`));
+    logger.info(
+      'Setup bin file for',
+      color.green(this.name),
+      color.dim(`(${this.binFilePath.split('node_modules/')[1]})`),
+    );
 
-    const binFileContent = readFileSync(this.binFilePath, "utf-8");
+    const binFileContent = readFileSync(this.binFilePath, 'utf-8');
 
     if (!binFileContent.includes(startConsole)) {
-      const lines = binFileContent.split("\n");
+      const lines = binFileContent.split('\n');
       lines.splice(1, 0, startConsole);
-      writeFileSync(this.binFilePath, lines.join("\n"));
+      writeFileSync(this.binFilePath, lines.join('\n'));
     }
   }
 
@@ -56,17 +67,17 @@ class BuildTool {
     logger.start(`Running start command: ${color.yellow(this.startScript)}`);
     return new Promise((resolve, reject) => {
       const child = spawn(`node --run ${this.startScript}`, {
-        stdio: ["pipe"],
+        stdio: ['pipe'],
         shell: true,
         env: {
           ...process.env,
-          NO_COLOR: "1",
+          NO_COLOR: '1',
         },
       });
       this.child = child;
       let startTime = null;
 
-      child.stdout.on("data", (data) => {
+      child.stdout.on('data', (data) => {
         const text = data.toString();
 
         if (process.env.DEBUG) {
@@ -81,7 +92,7 @@ class BuildTool {
         const match = this.startedRegex.exec(text);
         if (match) {
           if (!startTime) {
-            throw new Error("Start time not found");
+            throw new Error('Start time not found');
           }
           const time = Date.now() - startTime;
 
@@ -89,18 +100,18 @@ class BuildTool {
         }
       });
 
-      child.stderr.on("data", (data) => {
+      child.stderr.on('data', (data) => {
         console.error(`stderr: ${data}`);
       });
 
-      child.on("error", (error) => {
+      child.on('error', (error) => {
         console.log(`error: ${error.message}`);
         reject(error);
       });
-      child.on("exit", (code) => {
+      child.on('exit', (code) => {
         if (code !== 0 && code !== null) {
           console.log(
-            `(${this.name} run ${this.startScript} failed) child process exited with code ${code}`
+            `(${this.name} run ${this.startScript} failed) child process exited with code ${code}`,
           );
           reject(code);
         }
@@ -123,127 +134,128 @@ class BuildTool {
     logger.log('');
     logger.start(`Running build command: ${color.yellow(this.buildScript)}`);
     const child = spawn(`node --run ${this.buildScript}`, {
-      stdio: ["pipe"],
+      stdio: ['pipe'],
       shell: true,
       env: {
         ...process.env,
-        NO_COLOR: "1",
+        NO_COLOR: '1',
       },
     });
     const startTime = Date.now();
     return new Promise((resolve, reject) => {
-      child.on("exit", (code) => {
+      child.on('exit', (code) => {
         if (code === 0) {
           resolve(Date.now() - startTime);
         } else {
           reject(new Error(`Build failed with exit code ${code}`));
         }
       });
-      child.on("error", reject);
+      child.on('error', reject);
     });
   }
 }
 
-const allTools = [
-  'rsbuild',
-  'rspack',
-  'vite',
-  'webpack',
-  'farm',
-];
-// Failed to run Farm in GitHub Actions
-// so it is excluded from the default tools
-const defaultTools = [
-  'rsbuild',
-  'rspack',
-  'vite',
-  'webpack',
-];
-const toolNames = process.env.TOOLS === 'all' ? allTools : process.env.TOOLS?.split(',').map(item => item.toLowerCase()) || defaultTools;
+const parseToolNames = () => {
+  const allTools = ['rsbuild', 'rspack', 'vite', 'webpack', 'farm'];
+
+  if (process.env.TOOLS === 'all') {
+    return allTools;
+  }
+  if (process.env.TOOLS) {
+    return process.env.TOOLS?.split(',').map((item) => item.toLowerCase());
+  }
+
+  // Failed to run Farm in GitHub Actions
+  // so it is excluded from the default tools
+  const defaultTools = ['rsbuild', 'rspack', 'vite', 'webpack'];
+  return defaultTools;
+};
+
+const toolNames = parseToolNames();
 const buildTools = [];
 
-toolNames.forEach(name => {
+toolNames.forEach((name) => {
   switch (name) {
     case 'rspack':
       buildTools.push(
         new BuildTool({
-          name: "Rspack CLI " + require("@rspack/core/package.json").version,
+          name: 'Rspack CLI ' + require('@rspack/core/package.json').version,
           port: 8080,
-          startScript: "start:rspack",
+          startScript: 'start:rspack',
           startedRegex: /in (.+) (s|ms)/,
-          buildScript: "build:rspack",
-          binFilePath: "@rspack/cli/bin/rspack.js"
+          buildScript: 'build:rspack',
+          binFilePath: '@rspack/cli/bin/rspack.js',
         }),
         new BuildTool({
-          name: "Rspack CLI (Lazy) " +
-            require("@rspack/core/package.json").version,
+          name:
+            'Rspack CLI (Lazy) ' + require('@rspack/core/package.json').version,
           port: 8080,
-          startScript: "start:rspack:lazy",
+          startScript: 'start:rspack:lazy',
           startedRegex: /in (.+) (s|ms)/,
-          buildScript: "build:rspack",
-          binFilePath: "@rspack/cli/bin/rspack.js"
+          buildScript: 'build:rspack',
+          binFilePath: '@rspack/cli/bin/rspack.js',
         }),
       );
       break;
     case 'rsbuild':
       buildTools.push(
         new BuildTool({
-          name: "Rsbuild " + require("@rsbuild/core/package.json").version,
+          name: 'Rsbuild ' + require('@rsbuild/core/package.json').version,
           port: 3000,
-          startScript: "start:rsbuild",
+          startScript: 'start:rsbuild',
           startedRegex: /in (.+) (s|ms)/,
-          buildScript: "build:rsbuild",
-          binFilePath: "@rsbuild/core/bin/rsbuild.js",
+          buildScript: 'build:rsbuild',
+          binFilePath: '@rsbuild/core/bin/rsbuild.js',
         }),
         new BuildTool({
-          name: "Rsbuild (Lazy) " +
-            require("@rsbuild/core/package.json").version,
+          name:
+            'Rsbuild (Lazy) ' + require('@rsbuild/core/package.json').version,
           port: 3000,
-          startScript: "start:rsbuild:lazy",
+          startScript: 'start:rsbuild:lazy',
           startedRegex: /in (.+) (s|ms)/,
-          buildScript: "build:rsbuild",
-          binFilePath: "@rsbuild/core/bin/rsbuild.js"
-        })
+          buildScript: 'build:rsbuild',
+          binFilePath: '@rsbuild/core/bin/rsbuild.js',
+        }),
       );
       break;
     case 'vite':
       buildTools.push(
         new BuildTool({
-          name: "Vite (SWC) " + require("vite/package.json").version,
+          name: 'Vite (SWC) ' + require('vite/package.json').version,
           port: 5173,
-          startScript: "start:vite",
+          startScript: 'start:vite',
           startedRegex: /ready in (\d+) (s|ms)/,
-          buildScript: "build:vite",
-          binFilePath: "vite/bin/vite.js"
+          buildScript: 'build:vite',
+          binFilePath: 'vite/bin/vite.js',
         }),
       );
       break;
     case 'webpack':
       buildTools.push(
         new BuildTool({
-          name: "webpack (SWC) " + require("webpack/package.json").version,
+          name: 'webpack (SWC) ' + require('webpack/package.json').version,
           port: 8082,
-          startScript: "start:webpack",
+          startScript: 'start:webpack',
           startedRegex: /compiled .+ in (.+) (s|ms)/,
-          buildScript: "build:webpack",
-          binFilePath: "webpack-cli/bin/cli.js"
+          buildScript: 'build:webpack',
+          binFilePath: 'webpack-cli/bin/cli.js',
         }),
       );
       break;
     case 'farm':
       buildTools.push(
         new BuildTool({
-          name: "Farm " + require("@farmfe/core/package.json").version,
+          name: 'Farm ' + require('@farmfe/core/package.json').version,
           port: 9000,
-          startScript: "start:farm",
+          startScript: 'start:farm',
           startedRegex: /Ready in (.+)(s|ms)/,
-          buildScript: "build:farm",
-          binFilePath: "@farmfe/cli/bin/farm.mjs"
-        })
+          buildScript: 'build:farm',
+          binFilePath: '@farmfe/cli/bin/farm.mjs',
+        }),
       );
       break;
   }
-})
+});
 
 const browser = await puppeteer.launch();
 const warmupTimes = Number(process.env.WARMUP_TIMES) || 1;
@@ -253,12 +265,12 @@ const totalTimes = warmupTimes + runTimes;
 logger.log('');
 logger.start(
   'Benchmark case ' +
-  color.green(`"${caseName}"`) +
-  ' will run ' +
-  color.green(warmupTimes + ' warmup') +
-  ' + ' +
-  color.green(runTimes + ' measured') +
-  ' times'
+    color.green(`"${caseName}"`) +
+    ' will run ' +
+    color.green(warmupTimes + ' warmup') +
+    ' + ' +
+    color.green(runTimes + ' measured') +
+    ' times',
 );
 
 let totalResults = [];
@@ -275,12 +287,9 @@ async function runBenchmark() {
     const page = await browser.newPage();
     const start = Date.now();
 
-    page.on("load", () => {
+    page.on('load', () => {
       const loadTime = Date.now() - start;
-      console.log(
-        buildTool.name,
-        "startup time: " + (time + loadTime) + "ms"
-      );
+      console.log(buildTool.name, 'startup time: ' + (time + loadTime) + 'ms');
 
       if (!results[buildTool.name]) {
         results[buildTool.name] = {};
@@ -291,7 +300,7 @@ async function runBenchmark() {
       results[buildTool.name].onLoad = loadTime;
     });
 
-    console.log("Navigating to", `http://localhost:${buildTool.port}`);
+    console.log('Navigating to', `http://localhost:${buildTool.port}`);
 
     await page.goto(`http://localhost:${buildTool.port}`, {
       timeout: 60000,
@@ -305,25 +314,25 @@ async function runBenchmark() {
     let hmrRootStart = -1;
     let hmrLeafStart = -1;
 
-    page.on("console", (event) => {
+    page.on('console', (event) => {
       const isFinished = () => {
         return (
           results[buildTool.name]?.rootHmr && results[buildTool.name]?.leafHmr
         );
       };
-      if (event.text().includes("root hmr")) {
+      if (event.text().includes('root hmr')) {
         const clientDateNow = /(\d+)/.exec(event.text())[1];
         const hmrTime = clientDateNow - hmrRootStart;
-        console.log(buildTool.name, " Root HMR time: " + hmrTime + "ms");
+        console.log(buildTool.name, ' Root HMR time: ' + hmrTime + 'ms');
 
         results[buildTool.name].rootHmr = hmrTime;
         if (isFinished()) {
           page.close();
           waitResolve();
         }
-      } else if (event.text().includes("leaf hmr")) {
+      } else if (event.text().includes('leaf hmr')) {
         const hmrTime = Date.now() - hmrLeafStart;
-        console.log(buildTool.name, " Leaf HMR time: " + hmrTime + "ms");
+        console.log(buildTool.name, ' Leaf HMR time: ' + hmrTime + 'ms');
         results[buildTool.name].leafHmr = hmrTime;
         if (isFinished()) {
           page.close();
@@ -333,8 +342,8 @@ async function runBenchmark() {
     });
 
     await new Promise((resolve) => setTimeout(resolve, 1000));
-    const rootFilePath = path.join(__dirname, "src", caseName, "f0.jsx");
-    const originalRootFileContent = readFileSync(rootFilePath, "utf-8");
+    const rootFilePath = path.join(__dirname, 'src', caseName, 'f0.jsx');
+    const originalRootFileContent = readFileSync(rootFilePath, 'utf-8');
 
     appendFile(
       rootFilePath,
@@ -344,13 +353,18 @@ async function runBenchmark() {
       (err) => {
         if (err) throw err;
         hmrRootStart = Date.now();
-      }
+      },
     );
 
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    const leafFilePath = path.join(__dirname, "src", caseName, "d0/d0/d0/f0.jsx");
-    const originalLeafFileContent = readFileSync(leafFilePath, "utf-8");
+    const leafFilePath = path.join(
+      __dirname,
+      'src',
+      caseName,
+      'd0/d0/d0/f0.jsx',
+    );
+    const originalLeafFileContent = readFileSync(leafFilePath, 'utf-8');
     appendFile(
       leafFilePath,
       `
@@ -359,7 +373,7 @@ async function runBenchmark() {
       (err) => {
         if (err) throw err;
         hmrLeafStart = Date.now();
-      }
+      },
     );
 
     await waitPromise;
@@ -371,10 +385,12 @@ async function runBenchmark() {
     buildTool.stopServer();
 
     await new Promise((resolve) => setTimeout(resolve, 500));
-    logger.success("Dev server closed");
+    logger.success('Dev server closed');
 
     const buildTime = await buildTool.build();
-    logger.success(buildTool.name + ' built in ' + color.green(buildTime + ' ms'));
+    logger.success(
+      buildTool.name + ' built in ' + color.green(buildTime + ' ms'),
+    );
     results[buildTool.name].prodBuild = buildTime;
     await new Promise((resolve) => setTimeout(resolve, 500));
   }
@@ -406,13 +422,13 @@ for (const result of totalResults) {
 
 for (const [name, values] of Object.entries(averageResults)) {
   for (const [key, value] of Object.entries(values)) {
-    averageResults[name][key] = Math.floor(value / totalResults.length) + "ms";
+    averageResults[name][key] = Math.floor(value / totalResults.length) + 'ms';
   }
 }
 
 logger.log('');
 logger.success('Benchmark finished');
-logger.success("Average results of " + totalResults.length + " runs:");
+logger.success('Average results of ' + totalResults.length + ' runs:');
 console.table(averageResults);
 
 process.exit(0);
