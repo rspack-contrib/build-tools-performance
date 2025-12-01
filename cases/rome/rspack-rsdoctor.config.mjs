2d4b9e183934de67ac638f1044ea00e938f8d29a
easy1090 <congzhe@bytedance.com>
2025-12-01 16:41:00 +0800

    chore(CI): replace the rspack diff demos

--- cases/rome/rspack-rsdoctor.config.mjs
+++ cases/rome/rspack-rsdoctor.config.mjs
@@ -1,4 +1,7 @@
-
+/**
+ * Rsdoctor analysis configuration file
+ * This configuration is to support the CI process of the Rspack repository diff actions.
+ */
 import { defineConfig } from '@rspack/cli';
 import { RsdoctorPluginConfig } from '../../shared/rsdoctor.mjs'
 
