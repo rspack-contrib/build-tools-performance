cb5247b958149bead5297e3a0fcf1a6a83bec972
neverland <chenjiahan.jait@bytedance.com>
2026-01-23 10:30:06 +0800

    feat(deps): update Rspack to 2.0.0-alpha.0

--- shared/rspack.config.mjs
+++ shared/rspack.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from '@rspack/cli';
-import { isProd, target, targetBrowser } from './constants.mjs';
-import { rspack } from '@rspack/core';
+import { isProd, target } from './constants.mjs';
 
 export default defineConfig({
   devtool: isProd ? false : undefined,
@@ -9,31 +8,16 @@ export default defineConfig({
   resolve: {
     extensions: ['...', '.tsx', '.ts', '.jsx'],
   },
-  optimization: {
-    minimizer: [
-      new rspack.SwcJsMinimizerRspackPlugin(),
-      new rspack.LightningCssMinimizerRspackPlugin({
-        minimizerOptions: {
-          targets: [targetBrowser],
-        },
-      }),
-    ],
-  },
   module: {
     defaultRules: [
       '...',
-      // compat rspack2.0
       {
         test: /\.css$/,
         type: 'css/auto',
       },
     ],
   },
-  cache: true,
-  experiments: {
-    css: true,
-    cache: {
-      type: 'persistent',
-    },
+  cache: {
+    type: 'persistent',
   },
 });
