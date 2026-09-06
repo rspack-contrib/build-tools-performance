2596e9d15d13289058685761764be68cf3521bab
Sebastian Beltran <bjohansebas@gmail.com>
2026-09-06 16:33:56 +0000

    refactor: remove unused optimization plugins from webpack config

--- shared/webpack.config.mjs
+++ shared/webpack.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
 import { isProd, targetBrowser } from './constants.mjs';
-import TerserPlugin from 'terser-webpack-plugin';
-import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
-import { browserslistToTargets } from 'lightningcss';
-import browserslist from 'browserslist';
 
 export default {
   devtool: isProd ? false : undefined,
@@ -14,22 +10,6 @@ export default {
   cache: {
     type: 'filesystem',
     name: `webpack-web-${isProd ? 'prod' : 'dev'}`,
-  },
-  optimization: {
-    minimizer: isProd
-      ? [
-          new CssMinimizerPlugin({
-            minify: CssMinimizerPlugin.lightningCssMinify,
-            minimizerOptions: {
-              // @ts-ignore
-              targets: browserslistToTargets(browserslist(targetBrowser)),
-            },
-          }),
-          new TerserPlugin({
-            minify: TerserPlugin.swcMinify,
-          }),
-        ]
-      : [],
   },
   experiments: {
     css: true,
